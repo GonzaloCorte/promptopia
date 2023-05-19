553acdf11929f097d195f67aec130cc060e450e5
GonzaloCorte <gonzaacorte@mail.com>
2023-05-19 09:35:23 +0200

    added user profile view

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -1,16 +1,38 @@
 "use client";
 
 import React, { useState, useEffect } from 'react';
-import { useSearchParams } from 'next/navigation';
+import { useSearchParams } from "next/navigation";
 
 import Profile from '@components/Profile';
 
 
 const UserProfile = ({ params }) => {
   const searchParams = useSearchParams();
-  const username = searchParams('name');
+  const username = searchParams.get('name');
+
+  // const searchParams = useSearchParams();
+  // const username = searchParams.get("name");
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch(`/api/users/${params?.id}/posts`)
+      const data = await res.json();
+
+      setPosts(data);
+
+    }
+    if (params?.id) fetchPosts();
+
+  }, [params.id]);
+
   return (
-    <div>User Profile</div>
+    <Profile
+      name = {username}
+      desc = {'You may find interesting things around here'}
+      data = {posts}
+    />
   )
 }
 
